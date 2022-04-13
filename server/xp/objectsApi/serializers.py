e8d77f47d7848d81c0c22245c67bb0d0b3d69731
Cripson23 <101933529+Cripson23@users.noreply.github.com>
2022-04-13 17:28:09 +0500

    Delete server/xp directory

--- server/xp/objectsApi/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Objects
-
-
-class ObjectsSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Objects
-        fields = ('idObject', 'nameObject', 'descriptionObject', 'xObject', 'yObject')
--- server/xp/objectsApi/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from .serializers import ObjectsSerializer
-from .models import Objects
-
-
-class ObjectsViewSet(viewsets.ModelViewSet):
-    queryset = Objects.objects.all().order_by('-idObject')
-    serializer_class = ObjectsSerializer
