b45f200b42e6350d861f4d7a8cdb8a53cc3a3a10
Cripson23 <shepovalov.yaroslav@yandex.ru>
2022-04-13 18:22:19 +0500

    upd

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
 import pyrebase
 from config import config
 from flask import Flask, jsonify, abort, make_response, request, url_for
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 def db_connect():
@@ -16,6 +19,7 @@ db = db_connect()
 
 
 @app.route('/api/objects', methods=['GET'])
+@cross_origin()
 def get_objects():
     all_objects = db.child("objects").get()
 
@@ -29,6 +33,7 @@ def get_objects():
 
 
 @app.route('/api/objects/<string:obj_id>', methods=['GET'])
+@cross_origin()
 def get_object(obj_id):
     obj = db.child("objects").child(obj_id).get()
     if not obj.val():
@@ -41,6 +46,7 @@ def get_object(obj_id):
 
 
 @app.route('/api/objects', methods=['POST'])
+@cross_origin()
 def create_object():
     if not request.json or 'name' not in request.json or 'xObject' not in request.json or 'yObject' not in request.json \
             or 'descriptionObject' not in request.json:
@@ -60,6 +66,7 @@ def create_object():
 
 
 @app.route('/api/objects/<string:obj_id>', methods=['PUT'])
+@cross_origin()
 def update_object(obj_id):
     obj = db.child("objects").child(obj_id).get()
     if not obj.val():
@@ -82,6 +89,7 @@ def update_object(obj_id):
 
 
 @app.route('/api/objects/<string:obj_id>', methods=['DELETE'])
+@cross_origin()
 def delete_object(obj_id):
     obj = db.child("objects").child(obj_id).get()
     if not obj.val():
@@ -93,6 +101,7 @@ def delete_object(obj_id):
 
 
 @app.errorhandler(404)
+@cross_origin()
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
