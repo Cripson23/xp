45a6a38158aad6a95ad7c49acd7f850be3170429
Cripson23 <shepovalov.yaroslav@yandex.ru>
2022-04-15 15:38:25 +0500

    upd

--- server/app.py
+++ server/app.py
@@ -235,7 +235,7 @@ def upload_object_img(obj_id):
     return jsonify({'result': True})
 
 
-# Получение всех прошедших изображений объекта
+# Получение всех изображений объекта
 @app.route('/api/objects/<string:obj_id>/images/', methods=['GET'])
 @auth.login_required
 @cross_origin()
